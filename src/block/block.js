8f6a0c1d9371b41f901e27c4ff5128d7957e8483
Karen Neumann <karen@websavvystudio.com>
2019-02-23 10:36:06 -0500

    Formatting changes
    double into single quotes

--- src/block/block.js
+++ src/block/block.js
@@ -3,8 +3,8 @@
  */
 
 //  Import CSS.
-import "./style.scss";
-import "./editor.scss";
+import './style.scss';
+import './editor.scss';
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
@@ -14,61 +14,57 @@ const { CheckboxControl, RadioControl, TextControl } = wp.components;
 /**
  * Register block
  */
-registerBlockType("rescue-me/dog-block", {
-	title: __("Rescue Me - Dog Block"),
-	icon: "heart",
-	category: "common",
-	keywords: [
-		__("Rescue Me — Dog Block"),
-		__("Dog Block"),
-		__("Rescue Dog Block")
-	],
+registerBlockType('rescue-me/dog-block', {
+	title: __('Rescue Me - Dog Block'),
+	icon: 'heart',
+	category: 'common',
+	keywords: [__('Rescue Me — Dog Block'), __('Dog Block'), __('Rescue Dog Block')],
 	// meta fields are set in init.php
 	attributes: {
 		metaDogs: {
-			type: "boolean",
-			source: "meta",
-			meta: "rescue_me_meta_good_with_dogs"
+			type: 'boolean',
+			source: 'meta',
+			meta: 'rescue_me_meta_good_with_dogs'
 		},
 		metaCats: {
-			type: "boolean",
-			source: "meta",
-			meta: "rescue_me_meta_good_with_cats"
+			type: 'boolean',
+			source: 'meta',
+			meta: 'rescue_me_meta_good_with_cats'
 		},
 		metaKids: {
-			type: "boolean",
-			source: "meta",
-			meta: "rescue_me_meta_good_with_kids"
+			type: 'boolean',
+			source: 'meta',
+			meta: 'rescue_me_meta_good_with_kids'
 		},
 		metaGender: {
-			type: "string",
-			source: "meta",
-			meta: "rescue_me_meta_gender"
+			type: 'string',
+			source: 'meta',
+			meta: 'rescue_me_meta_gender'
 		},
 		metaSize: {
-			type: "string",
-			source: "meta",
-			meta: "rescue_me_meta_size"
+			type: 'string',
+			source: 'meta',
+			meta: 'rescue_me_meta_size'
 		},
 		metaAge: {
-			type: "string",
-			source: "meta",
-			meta: "rescue_me_meta_age"
+			type: 'string',
+			source: 'meta',
+			meta: 'rescue_me_meta_age'
 		},
 		contactName: {
-			type: "string",
-			source: "html",
-			selector: ".rm-contact-name"
+			type: 'string',
+			source: 'html',
+			selector: '.rm-contact-name'
 		},
 		contactEmail: {
-			type: "string",
-			source: "html",
-			selector: ".rm-contact-email"
+			type: 'string',
+			source: 'html',
+			selector: '.rm-contact-email'
 		},
 		contactPhone: {
-			type: "string",
-			attribute: "html",
-			selector: ".rm-contact-phone"
+			type: 'string',
+			attribute: 'html',
+			selector: '.rm-contact-phone'
 		}
 	},
 
@@ -93,74 +89,71 @@ registerBlockType("rescue-me/dog-block", {
 			<div className={className}>
 				<h4>Dog's characteristics</h4>
 				<CheckboxControl
-					heading={__("Good with", "rescue-me")}
-					label={__("Good with Dogs", "rescue-me")}
+					heading={__('Good with', 'rescue-me')}
+					label={__('Good with Dogs', 'rescue-me')}
 					checked={metaDogs}
 					onChange={metaDogs => {
 						setAttributes({ metaDogs });
 					}}
 				/>
 				<CheckboxControl
-					label={__("Good with Cats", "rescue-me")}
+					label={__('Good with Cats', 'rescue-me')}
 					checked={metaCats}
 					onChange={metaCats => {
 						setAttributes({ metaCats });
 					}}
 				/>
 				<CheckboxControl
-					label={__("Good with Kids", "rescue-me")}
+					label={__('Good with Kids', 'rescue-me')}
 					checked={metaKids}
 					onChange={metaKids => {
 						setAttributes({ metaKids });
 					}}
 				/>
 				<RadioControl
-					label={__("Gender", "rescue-me")}
+					label={__('Gender', 'rescue-me')}
 					selected={metaGender}
-					options={[
-						{ label: "Female", value: "Female" },
-						{ label: "Male", value: "Male" }
-					]}
+					options={[{ label: 'Female', value: 'Female' }, { label: 'Male', value: 'Male' }]}
 					onChange={metaGender => setAttributes({ metaGender })}
 				/>
 				<RadioControl
-					label={__("Size", "rescue-me")}
+					label={__('Size', 'rescue-me')}
 					selected={metaSize}
 					options={[
-						{ label: "Small", value: "Small" },
-						{ label: "Medium", value: "Medium" },
-						{ label: "Large", value: "Large" },
-						{ label: "Extra Large", value: "Extra Large" }
+						{ label: 'Small', value: 'Small' },
+						{ label: 'Medium', value: 'Medium' },
+						{ label: 'Large', value: 'Large' },
+						{ label: 'Extra Large', value: 'Extra Large' }
 					]}
 					onChange={metaSize => setAttributes({ metaSize })}
 				/>
 				<RadioControl
-					label={__("Age", "rescue-me")}
+					label={__('Age', 'rescue-me')}
 					selected={metaAge}
 					options={[
-						{ label: "Puppy", value: "Puppy" },
-						{ label: "Young", value: "Young" },
-						{ label: "Adult", value: "Adult" },
-						{ label: "Senior", value: "Senior" }
+						{ label: 'Puppy', value: 'Puppy' },
+						{ label: 'Young', value: 'Young' },
+						{ label: 'Adult', value: 'Adult' },
+						{ label: 'Senior', value: 'Senior' }
 					]}
 					onChange={metaAge => setAttributes({ metaAge })}
 				/>
 				<h4>Dog's contact info</h4>
 				<TextControl
 					className="rm-contact-name"
-					label={__("Contact name", "rescue-me")}
+					label={__('Contact name', 'rescue-me')}
 					value={contactName}
 					onChange={contactName => setAttributes({ contactName })}
 				/>
 				<TextControl
 					className="rm-contact-email"
-					label={__("Contact email", "rescue-me")}
+					label={__('Contact email', 'rescue-me')}
 					value={contactEmail}
 					onChange={contactEmail => setAttributes({ contactEmail })}
 				/>
 				<TextControl
 					className="rm-contact-phone"
-					label={__("Contact phone", "rescue-me")}
+					label={__('Contact phone', 'rescue-me')}
 					value={contactPhone}
 					onChange={contactPhone => setAttributes({ contactPhone })}
 				/>
@@ -179,13 +172,13 @@ registerBlockType("rescue-me/dog-block", {
 				<p className="rm-contact-name">{contactName}</p>
 
 				<p>
-					<a className="rm-contact-email" href={"mailto:" + contactEmail}>
+					<a className="rm-contact-email" href={'mailto:' + contactEmail}>
 						{contactEmail}
 					</a>
 				</p>
 
 				<p>
-					<a className="rm-contact-phone" href={"tel:" + contactPhone}>
+					<a className="rm-contact-phone" href={'tel:' + contactPhone}>
 						{contactPhone}
 					</a>
 				</p>
